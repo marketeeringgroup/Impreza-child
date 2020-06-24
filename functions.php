d139100b2be660e32c39a5b57e13502245bc9e4a
Kristinmahyout <Kydelv@gmail.com>
2020-06-24 12:57:29 -0700

    changed the functions file with a dumb comment.

--- functions.php
+++ functions.php
@@ -10,13 +10,5 @@ function imprezachild_enqueue_styles()
         array('parenthandle'),
         wp_get_theme()->get('Version') // this only works if you have Version in the style header
     );
-}
-
-/* Remove Menu Pages from Dashboard */
-add_action('admin_menu', 'imprezachild_remove_menus', 110);
-function imprezachild_remove_menus()
-{
-    remove_submenu_page('themes.php', 'theme-editor.php'); // Theme Editor
-}
-
-/* Mick was here Muaahahah */
+} 
+/* Kristin was here Muahaha*/
